53d1214e3367f2840dce331f3c361215813311c6
simk395 <simon.kong95@gmail.com>
2019-03-05 11:29:46 -0500

    on
    Signed-off-by: simk395 <simon.kong95@gmail.com>

---
+++ boilerplate-master/data/map/sprite.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="sprite" tilewidth="32" tileheight="32" tilecount="32" columns="16">
+ <image source="../img/sprite/gripe_run_right.png" width="512" height="64"/>
+</tileset>
--- boilerplate-master/js/game.js
+++ boilerplate-master/js/game.js
@@ -12,7 +12,7 @@ var game = {
     // Run on page load.
     "onload" : function () {
         // Initialize the video.
-        if (!me.video.init(900, 600, {wrapper : "screen", scale : "auto"})) {
+        if (!me.video.init(960, 640, {wrapper : "screen", scale : "auto", scalemethod: "flex-width"})) {
             alert("Your browser does not support HTML5 canvas.");
             return;
         }
@@ -27,13 +27,22 @@ var game = {
 
     // Run on game resources loaded.
     "loaded" : function () {
-        me.state.set(me.state.MENU, new game.TitleScreen());
-        me.state.set(me.state.PLAY, new game.PlayScreen());
+       
+       // set the "Play/Ingame" Screen Object
+  me.state.set(me.state.PLAY, new game.PlayScreen());
 
-        // add our player entity in the entity pool
-        me.pool.register("mainPlayer", game.PlayerEntity);
+  // register our player entity in the object pool
+  me.pool.register("mainPlayer", game.PlayerEntity);
 
-        // Start the game.
-        me.state.change(me.state.PLAY);
+  // enable the keyboard
+  me.input.bindKey(me.input.KEY.LEFT,  "left");
+  me.input.bindKey(me.input.KEY.RIGHT, "right");
+  // map X, Up Arrow and Space for jump
+  me.input.bindKey(me.input.KEY.X,      "jump", true);
+  me.input.bindKey(me.input.KEY.UP,     "jump", true);
+  me.input.bindKey(me.input.KEY.SPACE,  "jump", true);
+
+  // start the game
+  me.state.change(me.state.PLAY);
     }
 };
