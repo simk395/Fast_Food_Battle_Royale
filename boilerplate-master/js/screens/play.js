47b853c16f91eea71e8d0542cc21410394614419
simk395 <simon.kong95@gmail.com>
2019-03-08 10:01:09 -0500

    electric fountain

--- boilerplate-master/js/screens/play.js
+++ boilerplate-master/js/screens/play.js
@@ -10,7 +10,7 @@ game.PlayScreen = me.ScreenObject.extend({
         // Can also be forced by specifying a "Infinity" z value to the addChild function.
         this.HUD = new game.HUD.Container();
         me.game.world.addChild(this.HUD);
-        me.audio.playTrack("dst-inertexponent")
+        me.audio.playTrack("Electric-Fountain")
     },
 
     /**
